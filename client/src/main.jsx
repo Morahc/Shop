e32cba31eb3e9b8f197c23bb315a43ddbda070f0
Morah Stephen <Morahstephen872@gmail.com>
2022-04-04 09:43:35 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ScrollToTop from './components/ScrollTotop';
 import AuthState from './context/auth/AuthState';
 import ProductState from './context/product/ProductState';
 import Notification from './components/Notification';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop />
     <React.StrictMode>
       <AuthState>
@@ -19,6 +19,6 @@ ReactDOM.render(
         </ProductState>
       </AuthState>
     </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
