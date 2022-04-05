2f30b55e5bf05420ca7f4322ecce19aed54c06ab
Morah Stephen <Morahstephen872@gmail.com>
2022-04-05 19:55:14 +0100

    updated Product

--- client/src/pages/Home/components/Hero.jsx
+++ client/src/pages/Home/components/Hero.jsx
@@ -4,10 +4,10 @@ const Hero = () => {
   const navigate = useNavigate()
   return (
     <section className='flex-grow px-6'>
-      <div className='h-full flex flex-wrap items-center justify-center'>
+      <div className='h-full flex items-center justify-center'>
         <div className='basis-full md:basis-1/2 p-6 space-y-8'>
-          <h1 className='text-6xl'>All New Phones up to 25% Flat Sale</h1>
-          <p className='text-lg'>
+          <h1 className='text-6xl md:text-8xl'>All New Phones up to 25% Flat Sale</h1>
+          <p className='text-base md:text-lg'>
             It is a long established fact that a reader will be distracted by the readable content
             of a page when looking at its layout. The point of using Lorem Ipsum is that
           </p>
